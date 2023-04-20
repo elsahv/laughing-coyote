fbeae85d0b97bc45cbaed55f621e385524b10e5b
Elsa <elsahvy@gmail.com>
2023-04-19 19:46:42 -0700

    contact adjust

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <section id="contact" className="">
+    <section id="contact" className="h-screen">
       <h2 className="bg-teal">Contact</h2>
       <p>
-        Has my portfolio piqued your interest...? If so, you can contact me at
+        Has my portfolio piqued your interest....? If so, you can contact me at
         <em>devdesignsbyelsa@gmail.com</em>
       </p>
       <p>Looking forward to hearing from you!</p>
