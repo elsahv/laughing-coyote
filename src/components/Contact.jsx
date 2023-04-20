7b54715c36bb7107c74e31852605ea9b00d7ffd8
Elsa <elsahvy@gmail.com>
2023-04-19 19:43:43 -0700

    contact adjust

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <section id="contact" className="">
       <h2 className="bg-teal">Contact</h2>
       <p>
-        Has my portfolio piqued your interest? If so, you can contact me at
+        Has my portfolio piqued your interest...? If so, you can contact me at
         <em>devdesignsbyelsa@gmail.com</em>
       </p>
       <p>Looking forward to hearing from you!</p>
